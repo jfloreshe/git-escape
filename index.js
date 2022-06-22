b90448c01d5bc33012ae0a1b0758dbe452ba66a6
jfloreshe <undrjeff@gmail.com>
2022-06-22 01:16:18 -0500

    script to fetch data from .git

--- index.js
+++ index.js
@@ -1,76 +1,113 @@
-const { exec, spawn } = require('child_process');
+const { spawn } = require('child_process');
 const fs  = require('fs');
 
 const GITPATH = process.argv[2];
 
 function gitRevList(){
-    let git = spawn('git',['log']);
     return new Promise((resolve, reject) => {
+        let buffer = "";
+        let git = spawn('git', [`--git-dir=${GITPATH}`, 'rev-list', '--all', '--parents']);
         git.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-            resolve(1);
+            buffer += data;
         });
           
         git.stderr.on('data', (data) => {
             console.error(`stderr: ${data}`);
-            reject(new Error(`stderr: ${data}`))
         });
         
         git.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
+            if (code != 0){
+                return reject(`git had an error trying to resolve rev-list with ${code}`)
+            }
+            resolve(buffer);
         });
     })
 }
 
-async function run(){
-    const response = await gitRevList();
+function gitExtraData(id){
+    return new Promise((resolve, reject) => {
+        let buffer = "";
+        let git = spawn('git', [`--git-dir=${GITPATH}`, 'log', '--numstat','--pretty=format:%an%n%ae%n%ai%n%s','-1', id])
+        git.stdout.on('data', (data) => {
+            buffer += data;
+        });
+
+        git.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+        
+        git.on('close', (code) => {
+            if (code != 0){
+                return reject(`git had an error trying to resolve log with ${code}`)
+            }
+            resolve(buffer);
+        });
+    })
+}
+
+
+function run(){
+    const nodes=[];
+    return new Promise(async(resolve, reject) => {
+        let response = await gitRevList();
+        let linesIds = response.split(/\r?\n/);
+        for(let line of linesIds){
+            if(line !== ''){
+                ids = line.split(' ');
+                let id = ids[0];
+                let parents = ids.slice(1);
+                let response = await gitExtraData(id);
+                let linesExtraData = response.split(/\r?\n/);
+                let autor = linesExtraData[0];
+                let email = linesExtraData[1];
+                let dateRaw = linesExtraData[2].split(' ');
+                let date = {
+                    yyyymmdd: dateRaw[0],
+                    time: dateRaw[1],
+                    timeZone: dateRaw[2]
+                }
+                let message = linesExtraData[3];
+                let filesRaw = linesExtraData.slice(4, linesExtraData.length - 1);
+                let files = [];
+                filesRaw.forEach( fileRaw => {
+                                let file = fileRaw.split(/\t/)
+                                let fileObj = {
+                                    insert: file[0],
+                                    delet: file[1],
+                                    full: file[2]
+                                }
+                                files.push(fileObj);
+                            });
+                let node = {
+                    id,
+                    parents,
+                    autor,
+                    email,
+                    date,
+                    message,
+                    files
+                }
+                nodes.push(node)
+            }
+        }
+        resolve(nodes);
+    })
+    
 }
 
 run()
-// function gitRevList(){
-//     return new Promise((resolve, reject) => {
-//         exec(`git --git-dir=${GITPATH} rev-list --all --parents > test.txt`, (err, stdout, sterr) =>{
-//             if (err){
-//                 reject(new Error(`error: ${err}`))
-//             }
-//             resolve(1);
-//         });        
-//     })
-// }
+    .then((response) => {
+        let jsonContent = JSON.stringify(response);
+        fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
+            if (err) {
+                console.log("An error occured while writing JSON Object to File.");
+                return console.log(err);
+            }
+         
+            console.log("JSON file has been saved.");
+        });
+    })
+    .catch((err) => {
+        console.error(new Error(`Error: ${err}`))
+    })
 
-// function gitExtraData(id){
-//     return new Promise((resolve, reject) => {
-//         exec(`git --git-dir=${GITPATH} `)
-//     })
-// }
-
-// gitRevList()
-//     .then((data) => {
-//         if (data === 1){
-//             let finalObject = {}
-//             let key = 'nodes';
-//             finalObject[key] = [];
-//             let sizeKey = 'size';
-//             finalObject[sizeKey] = 0; 
-//             const allFileContents = fs.readFileSync('test.txt', 'utf-8');
-//             allFileContents.split(/\r?\n/).forEach(line =>  {
-//                 ids = line.split(' ');
-//                 if(ids[0] === ""){
-//                     return;
-//                 }    
-//                 console.log(ids);
-//                 const newNode = {
-//                     id: ids[0],
-//                     parents: ids.slice(1)
-//                 }
-//                 finalObject[key].push(newNode);
-//                 finalObject[sizeKey]++;
-//             });         
-//             const json = JSON.stringify(finalObject);
-//             fs.writeFile("gitMetaData.json", json, function(err) {
-//                 if(err){
-//                     console.error(err);
-//                 }
-//             })
-//         }
-//     })
