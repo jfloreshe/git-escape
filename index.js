813624608f6693bb6c622bd1d766f4a3d89a7af8
jfloreshe <undrjeff@gmail.com>
2022-06-27 13:13:45 -0500

    se agrego metodo para obtener autores y el numero de commmits hechos en todo el proyecto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs  = require('fs');
 
 const GITPATH = process.argv[2];
 
-function gitRevList(){
+function getRawCommitsAndParents(){
     return new Promise((resolve, reject) => {
         let buffer = "";
         let git = spawn('git', [`--git-dir=${GITPATH}`, 'rev-list', '--all', '--parents']);
@@ -24,7 +24,7 @@ function gitRevList(){
     })
 }
 
-function gitExtraData(id){
+function getRawDataFromCommit(id){
     return new Promise((resolve, reject) => {
         let buffer = "";
         let git = spawn('git', [`--git-dir=${GITPATH}`, 'log', '--numstat','--pretty=format:%an%n%ae%n%ai%n%s','-1', id])
@@ -45,20 +45,19 @@ function gitExtraData(id){
     })
 }
 
-
-function run(){
-    const nodes=[];
+function getCommitNodes(){
+    const nodes = [];
     return new Promise(async(resolve, reject) => {
-        let response = await gitRevList();
+        let response = await getRawCommitsAndParents();
         let linesIds = response.split(/\r?\n/);
         for(let line of linesIds){
             if(line !== ''){
                 ids = line.split(' ');
                 let id = ids[0];
                 let parents = ids.slice(1);
-                let response = await gitExtraData(id);
+                let response = await getRawDataFromCommit(id);
                 let linesExtraData = response.split(/\r?\n/);
-                let autor = linesExtraData[0];
+                let author = linesExtraData[0];
                 let email = linesExtraData[1];
                 let dateRaw = linesExtraData[2].split(' ');
                 let date = {
@@ -81,7 +80,7 @@ function run(){
                 let node = {
                     id,
                     parents,
-                    autor,
+                    author,
                     email,
                     date,
                     message,
@@ -92,22 +91,89 @@ function run(){
         }
         resolve(nodes);
     })
-    
+}
+
+function getRawAuthors(){
+    return new Promise((resolve, reject) => {
+        let buffer = "";
+        let git = spawn('git', [`--git-dir=${GITPATH}`, 'shortlog', '-s', '--all'])
+        git.stdout.on('data', (data) => {
+            buffer += data;
+        });
+
+        git.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+        
+        git.on('close', (code) => {
+            if (code != 0){
+                return reject(`git had an error trying to resolve authors with ${code}`)
+            }
+            resolve(buffer);
+        });
+    })
+}
+
+function getAuthors(){
+    const authors = []
+    return new Promise(async(resolve, reject) => {
+        let response = await getRawAuthors();
+        let linesAuthors = response.split(/\r?\n/);
+        
+        linesAuthors.forEach(line => {
+            if(line !== ''){
+                let lineTrimmed = line.trim();
+                let author = lineTrimmed.split(/\t/)
+                let newObj = {
+                    author: author[1],
+                    commits: author[0]
+                };
+                authors.push(newObj);
+            }       
+        })
+        resolve(authors);
+    })
+}
+
+function getRawBranhches(){
+    return new Promise((resolve, reject) => {
+        let buffer = "";
+        let git = spawn('git', [`--git-dir=${GITPATH}`, 'branch', '-v', '--all'])
+        git.stdout.on('data', (data) => {
+            buffer += data;
+        });
+
+        git.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+        
+        git.on('close', (code) => {
+            if (code != 0){
+                return reject(`git had an error trying to resolve authors with ${code}`)
+            }
+            resolve(buffer);
+        });
+    })
+}
+
+async function run(){
+    const nodes = await getCommitNodes();
+    //recreateDirectory(nodes);
+    const authors = await getAuthors();
+    const jsonObj = {
+        nodes,
+        authors,
+    };
+
+    let jsonContent = JSON.stringify(jsonObj);
+    fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+        
+        console.log("JSON file has been saved.");
+    });
 }
 
 run()
-    .then((response) => {
-        let jsonContent = JSON.stringify(response);
-        fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
-            if (err) {
-                console.log("An error occured while writing JSON Object to File.");
-                return console.log(err);
-            }
-         
-            console.log("JSON file has been saved.");
-        });
-    })
-    .catch((err) => {
-        console.error(new Error(`Error: ${err}`))
-    })
-
